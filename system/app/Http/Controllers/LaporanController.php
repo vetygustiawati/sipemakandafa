538e82620e2d299e30f8f84baec63127374c1d4d
StartUpTech21 <71057826+vetygustiawati@users.noreply.github.com>
2021-06-28 11:32:03 +0700

    filter absen form widi

--- system/app/Http/Controllers/LaporanController.php
+++ system/app/Http/Controllers/LaporanController.php
@@ -59,31 +59,20 @@ class LaporanController extends Controller
 
     public function cariAdmin()
     {
-        $dari = request()->dari;
-        $sampai = request()->sampai;
-        $data['laporan_presensi'] = 
-        // Presensi::whereBetween('tgl_presensi', [$dari, $sampai])->get();
-        DB::table('presensi')->select('*','santri.id_santri','santri.nm_wsantri',
-            DB::raw("max(case when bln='01' THEN status else 0 end )as jan"),
-            DB::raw("max(case when bln='02' THEN status else 0 end )as feb"),
-            DB::raw("max(case when bln='03' THEN status else 0 end )as mar"),
-            DB::raw("max(case WHEN bln='04' THEN status ELSE 0 end) as apr"),
-            DB::raw("max(case WHEN bln='05' THEN status ELSE 0 end) as mei"),
-            DB::raw("max(case WHEN bln='06' THEN status ELSE 0 end) as jun"),
-            DB::raw("max(case WHEN bln='07' THEN status ELSE 0 end) as jul"),
-            DB::raw("max(case WHEN bln='08' THEN status ELSE 0 end) as agu"),
-            DB::raw("max(case WHEN bln='09' THEN status ELSE 0 end) as sep"),
-            DB::raw("max(case WHEN bln='10' THEN status ELSE 0 end) as okt"),
-            DB::raw("max(case WHEN bln='11' THEN status ELSE 0 end) as nov"),
-            DB::raw("max(case WHEN bln='12' THEN status ELSE 0 end) as des"))
+        $tahun = request()->tahun;
+        $bulan = request()->bulan;
+        $data['laporan_presensi'] = DB::table('presensi')
+        ->select('presensi.*','santri.id_santri','santri.nm_wsantri','santri.nama_santri')
         ->join('santri','santri.id_santri','presensi.id_santri')
-        ->whereBetween('tgl_presensi', [$dari, $sampai])
+        ->where('bln', $bulan)
+        ->where('tgl_presensi','like', $tahun.'%')
         ->get();
+        // dd($data['laporan_presensi']);
         return view('admin.laporan.index',$data);
     }
     public function cariPengasuh()
     {
-                $dari = request()->dari;
+        $dari = request()->dari;
         $sampai = request()->sampai;
         $data['laporan_presensi'] = 
         // Presensi::whereBetween('tgl_presensi', [$dari, $sampai])->get();
--- system/resources/views/admin/laporan/index.blade.php
+++ system/resources/views/admin/laporan/index.blade.php
@@ -15,21 +15,35 @@
 				<div class="card-body">
 				<form action="{{url('admin/laporanCari')}}" method="get">
 					<div class="col-sm-12">
-						<label class="col-sm-3 col-form-label">Dari Kapan</label>
+						<label class="col-sm-3 col-form-label">Pilih Tahun</label>
 						<div class="input-group mb-3">
 				               <div class="input-group-prepend">
 				               	<span class="input-group-text"><i class="fas fa-calendar"></i></span>
 				               </div>
-							<input type="date" name="dari" value="{{$dari ?? ''}}" id="" class="form-control">
+							<input type="text" name="tahun" id="" placeholder="2021" class="form-control">
 		               	</div>
 					</div>
 					<div class="col-sm-12">
-						<label class="col-sm-3 col-form-label">Sampai Kapan</label>
+						<label class="col-sm-3 col-form-label">Pilih Bulan</label>
 						<div class="input-group mb-3">
 				               <div class="input-group-prepend">
 				               	<span class="input-group-text"><i class="fas fa-calendar"></i></span>
 				               </div>
-							<input type="date" name="sampai" value="{{$sampai ?? ''}}" id="" class="form-control">
+				            <select name="bulan" class="form-control" required="">
+				            	<option>--Pilih Bulan--</option>
+				            	<option value="01">1-Januari</option>
+				            	<option value="02">2-Februari</option>
+				            	<option value="03">3-Maret</option>
+				            	<option value="04">4-April</option>
+				            	<option value="05">5-Mei</option>
+				            	<option value="06">6-Juni</option>
+				            	<option value="07">7-Juli</option>
+				            	<option value="08">8-Agustus</option>
+				            	<option value="09">9-September</option>
+				            	<option value="10">10-Oktober</option>
+				            	<option value="11">11-November</option>
+				            	<option value="12">12-Desember</option>
+				            </select>
 		               	</div>
 					</div>
 					<button type="input" class="btn btn-primary float-right">Cari</button>
@@ -56,120 +70,26 @@
 				</div>
 				<div class="card-body table-responsive">
 					<div class="table-responsive">
-						<table class="table table-datatable">
+						<table class="table table-datatable table-bordered table-sm">
 							<thead>
 								<th>NO</th>
 								<th>Nama Santri</th>
 								<th>Nama Wali Santri</th>
-								<th>Jan</th>
-								<th>Feb</th>
-								<th>Mar</th>
-								<th>Apr</th>
-								<th>Mei</th>
-								<th>Jun</th>
-								<th>Jul</th>
-								<th>Agt</th>
-								<th>Sep</th>
-								<th>Okt</th>
-								<th>Nov</th>
-								<th>Des</th>
+								<th>Tahun</th>
+								<th>Bulan</th>
+								<th>Status</th>
 							</thead>
 							<tbody>
-			                @foreach ($laporan_presensi as $d)
-							<tr>
-								<th>{{$loop->iteration}}</th>
-								<td>{{$d->nama_santri}}</a></td>
-								<td>{{$d->nm_wsantri}}</a></td>
-										<!-- jan -->
-										<td>
-											@if($d->jan == 'hadir')
-											<button class="btn btn-success"><i class="fa fa-check"></i></button>
-											@elseif($d->jan == '0')
-											<button class="btn btn-danger"><i class="fa fa-times"></i></button>
-											
-											@endif
-										</td>							<td><!-- feb -->
-								@if($d->feb == 'hadir')
-									<button class="btn btn-success"><i class="fa fa-check"></i></button>
-								@elseif($d->feb == '0')
-									<button class="btn btn-danger"><i class="fa fa-times"></i></button>
-									
-								@endif
-							</td>
-							<td><!-- mar -->
-								@if($d->mar == 'hadir')
-									<button class="btn btn-success"><i class="fa fa-check"></i></button>
-								@elseif($d->mar == '0')
-									<button class="btn btn-danger"><i class="fa fa-times"></i></button>
-								@endif
-							</td>
-							<td><!-- apr -->
-								
-							<!-- 	<h6>Ramadhan</h6> -->
-								@if($d->apr == 'hadir')
-									<button class="btn btn-success"><i class="fa fa-check"></i></button>
-								@elseif($d->apr == '0')
-									<button class="btn btn-danger"><i class="fa fa-times"></i></button>
-								@endif
-							</td>
-							<td><!-- mei -->
-								@if($d->mei == 'hadir')
-									<button class="btn btn-success"><i class="fa fa-check"></i></button>
-								@elseif($d->mei == '0')
-									<button class="btn btn-danger"><i class="fa fa-times"></i></button>
-								@endif
-							</td>
-							<td><!-- jun -->
-								@if($d->jun == 'hadir')
-									<button class="btn btn-success"><i class="fa fa-check"></i></button>
-								@elseif($d->jun == '0')
-									<button class="btn btn-danger"><i class="fa fa-times"></i></button>
-								@endif
-							</td>
-							<td><!-- jul -->
-								@if($d->jul == 'hadir')
-									<button class="btn btn-success"><i class="fa fa-check"></i></button>
-								@elseif($d->jul == '0')
-									<button class="btn btn-danger"><i class="fa fa-times"></i></button>
-								@endif
-							</td>
-							<td><!-- agu -->
-								@if($d->agu == 'hadir')
-									<button class="btn btn-success"><i class="fa fa-check"></i></button>
-								@elseif($d->agu == '0')
-									<button class="btn btn-danger"><i class="fa fa-times"></i></button>
-								@endif
-							</td>
-							<td><!-- sep -->
-								@if($d->sep == 'hadir')
-									<button class="btn btn-success"><i class="fa fa-check"></i></button>
-								@elseif($d->sep == '0')
-									<button class="btn btn-danger"><i class="fa fa-times"></i></button>
-								@endif
-							</td>
-							<td><!-- okt -->
-								@if($d->okt == 'hadir')
-									<button class="btn btn-success"><i class="fa fa-check"></i></button>
-								@elseif($d->okt == '0')
-									<button class="btn btn-danger"><i class="fa fa-times"></i></button>
-								@endif
-							</td>
-							<td><!-- nov -->
-								@if($d->nov == 'hadir')
-									<button class="btn btn-success"><i class="fa fa-check"></i></button>
-								@elseif($d->nov == '0')
-									<button class="btn btn-danger"><i class="fa fa-times"></i></button>
-								@endif
-							</td>
-							<td><!-- des -->
-								@if($d->des == 'hadir')
-									<button class="btn btn-success"><i class="fa fa-check"></i></button>
-								@elseif($d->des == '0')
-									<button class="btn btn-danger"><i class="fa fa-times"></i></button>
-								@endif
-							</td>
-									</tr>
-									@endforeach
+							@foreach($laporan_presensi as $d)
+								<tr>
+									<td>{{$loop->iteration}}</td>
+									<td>{{$d->nama_santri}}</td>
+									<td>{{$d->nm_wsantri}}</td>
+									<td>{{$d->tgl_presensi}}</td>
+									<td>{{$d->bln}}</td>
+									<td><span class="badge badge-pill badge-success">Hadir</span></td>
+								</tr>
+							@endforeach
 							</tbody>
 						</table>
 					</div>
